f09a41b66bb3179ea77b347609875111af18beb1
pevescarol <carol.peves@gmail.com>
2020-06-24 01:58:36 -0300

    primer commit

---
+++ js/galeria.js
@@ -0,0 +1,38 @@
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+let galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages){
+    galleryImages.forEach(function(image, index){
+        image.onclick = function(){
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = getFullImgUrl.split("/photos/thumbs/");
+            let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+
+            getLatestOpenedImg = index + 1;
+
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
+
+            let newImg = document.createElement("img");
+            newImgWindow.appendChild(newImg);
+            newImg.setAttribute("src", "photos/" + setNewImgUrl);
+            newImg.setAttribute("id", "current-img");
+        };
+    });
+}
+
+function closeImg(){
+    document.querySelector(".img-window").remove();
+}
